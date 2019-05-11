e22b8717c24d6aee7f8dc08335858b9ced04757b
Dong <black_dragon@foxmail.com>
2019-05-11 11:15:58 +0800

    基于子类的动态代理

---
+++ src/main/java/cn/imust/cglib/Producer.java
@@ -0,0 +1,25 @@
+package cn.imust.cglib;
+
+import cn.imust.proxy.IProducer;
+
+/**
+ * 某一个生产厂家
+ */
+public class Producer {
+
+    /**
+     * 销售
+     * @param money
+     */
+    public void saleProduct(float money){
+        System.out.println("销售产品,并拿到钱" + money);
+    }
+
+    /**
+     * 售后
+     * @param money
+     */
+    public void afterService(float money){
+        System.out.println("提供售后服务,并拿到钱" + money);
+    }
+}
